eb8006a342f9fbe150acc1af3d6eb497a80b1528
Steve Brownlee <chortlehoort@gmail.com>
2021-10-19 15:21:57 -0500

    Showing the name of a chosen clay

--- scripts/Clays.js
+++ scripts/Clays.js
@@ -1,6 +1,8 @@
 /*
     Responsibility:
         Generate the HTML representations of clays
+
+        State = Data as it exists right now
 */
 
 import { getClays } from "./database.js";
@@ -8,14 +10,38 @@ import { getClays } from "./database.js";
 // State variable (copy of the data)
 const allTheClays = getClays()
 
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.name === "clay") {
+            const primaryKey = changeEvent.target.value   // "4"
+
+            // find() array method is for finding a single item in the array
+            const theChosenClay = allTheClays.find(
+                (clayObj) => {
+                    if (parseInt(primaryKey) === clayObj.id) {
+                        return true
+                    }
+                    return false
+                }
+            )
+
+            window.alert(theChosenClay.clay)
+        }
+    }
+)
+
 export const ClayHTML = () => {
-    let html = ""
+    let html = "<header> <h2>Choose a clay</h2> </header>"
 
     // Iterate over our copy of the clay object array
     for (const clayObj of allTheClays) {
         // Build up a string while iterating (radio buttons for each clay)
         html += `
+        <div>
+            <input type="radio" name="clay" value="${clayObj.id}" />
             ${clayObj.clay}
+        </div>
         `
     }
 
